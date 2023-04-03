883c731509d51bb997f49186cd88a2ecaf18027f
phuc14022001 <phucdong1402@gmail.com>
2023-04-03 22:03:36 +0700

    phuc

---
+++ dmlcomputer/Common/ProductType.cs
@@ -0,0 +1,18 @@
+﻿namespace DoAn_LapTrinhWeb.Common
+{
+    public static class ProductType
+    {
+        public const int LAPTOP = 1;
+        public const int ACCESSORY = 3;
+        public const int PC = 2;
+    }
+    public static class ProductGene
+    {
+        public const int LAPTOPVANPHONG = 1;
+        public const int LAPTOPGAMING = 11;
+        public const int LAPTOPWORKSTATION = 15;
+        
+        public const int PCGAMING = 37;
+        public const int PCWORKSTATION = 36;
+    }
+}
