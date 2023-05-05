48f3868d99df11689a0e1fb196db9c6479069c86
phuc14022001 <phucdong1402@gmail.com>
2023-05-05 16:09:52 +0700

    a

--- dmlcomputer/Common/EmailConfig.cs
+++ dmlcomputer/Common/EmailConfig.cs
@@ -11,7 +11,7 @@ namespace DoAn_LapTrinhWeb.Common
            Để dùng gmail gửi email rs cho người khác bạn cần phải vô đây "https://www.google.com/settings/security/lesssecureapps" Cho phép ứng dụng kém an toàn: Bật
          */
         public const string emailID = "phucdong1402@gmail.com";
-        public const string emailPassword = "qvgfvywkfcjxilwx";
+        public const string emailPassword = "krrnbkyrkwcdcped";
         public const string emailHost = "smtp.gmail.com"; //nếu dùng gmail thì đổi thành  "Host = ""
         public const string emailName = "P Car";//Tên email hiển thị khi gửi
     }
